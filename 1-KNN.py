26a2cdfa5b684c43e5543441f19799ca7ddda7a2
SamanePahlavani <88037175+SaPahlavani@users.noreply.github.com>
2023-10-05 18:29:23 +0330

    Add files via upload

---
+++ 1-KNN.py
@@ -0,0 +1,100 @@
+#Load DataSet
+import pandas as pd
+import requests
+import io
+
+# URL to download the file from Google Drive (modified to get the direct download link)
+file_id = "11U_HpgZzzy7Jsvlpljsrtw5xL2X2dSSQ"
+url = f"https://drive.google.com/uc?id={file_id}"
+
+# Make a request to download the file
+response = requests.get(url)
+
+# Read the content of the downloaded file using Pandas
+df = pd.read_csv(io.StringIO(response.text))
+
+#Import 
+import os
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import warnings
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.tree import plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from pycm import ConfusionMatrix, Compare
+#Parameters
+FOLD_NUMBER = 10
+RANDOM_STATE = 23
+METRIC_LIST = ["Accuracy", "F1", "Kappa", "Precision", "Recall"]
+warnings.filterwarnings('ignore')
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+#Feature Seelection
+corr = df.iloc[:,2:].corr()
+mask = np.zeros_like(corr)
+mask[np.triu_indices_from(mask)]=True
+with sns.axes_style('white'):
+    fig, ax = plt.subplots(figsize=(18,10))
+    sns.heatmap(corr,  mask=mask, annot=False, cmap='CMRmap', center=0, square=True)
+    
+# Dataset Imbalanced we sampled negative cases randomly to create a new balanced dataset:
+
+positive_sample = df[df["Flag"] == 1]
+negative_sample = df[df["Flag"] == 0].sample(len(positive_sample), random_state = RANDOM_STATE)
+data = pd.concat([negative_sample, positive_sample], axis = 0)
+y = data["Flag"]
+X = data.iloc[:,2:]
+#Replace Nan with average:
+for col in X.columns:
+    X[col] = X[col].fillna(X[col].mean())
+
+#Parameters
+knn_params = {"n_neighbors": 15}
+#Train
+knn_model = KNeighborsClassifier(**knn_params)
+kf = KFold(n_splits=10, random_state = RANDOM_STATE, shuffle=True)
+cm_metric_list = []
+knn_cm_worst = None
+knn_cm_best = None
+knn_cm_list = []
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test =  scaler.transform(X_test)
+    knn_model.fit(X_train, y_train)
+    cm = ConfusionMatrix(actual_vector = y_test.values, predict_vector = knn_model.predict(X_test))
+    cm_metric_list.append({"Accuracy":cm.Overall_ACC, "F1": cm.F1[1], "Kappa": cm.Kappa, "Precision":cm.PPV[1]
+                        , "Recall": cm.TPR[1]})
+    cm.relabel({1: "Fraud", 0:"Non-Fraud"})
+    knn_cm_list.append(cm)
+    if knn_cm_worst is None:
+        knn_cm_worst = cm
+    else:
+        if cm.Overall_ACC < knn_cm_worst.Overall_ACC:
+            knn_cm_worst = cm
+
+    if knn_cm_best is None:
+        knn_cm_best = cm
+    else:
+        if cm.Overall_ACC > knn_cm_best.Overall_ACC:
+            knn_cm_best = cm
+#Performance
+
+print("Model Name: KNN Classifier\n")
+print("10-Fold Metrics: \n")
+for metric in METRIC_LIST:
+    temp = []
+    for item in cm_metric_list:
+        temp.append(item[metric])
+    print("{0} : {1}\n".format(metric, np.mean(temp).round(2)))
+knn_cm_worst.plot(title="KNN Worst", number_label=True)
+knn_cm_best.plot(title="KNN Best", number_label=True)
+plt.show()
